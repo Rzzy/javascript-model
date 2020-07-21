4bd52b9b48c0acf782c740fee4652f3602e71f60
Rz <243566431@qq.com>
2020-07-21 21:34:55 +0800

    update

--- 装饰者模式.js
+++ 装饰者模式.js
@@ -62,3 +62,56 @@ sale = sale.decorate('money')
 sale instanceof Sale
 sale.getPrice()  // $112.88
 
+// 使用列表实现
+function Sale(price){
+    this.price = price || 100
+    this.decorators_list = []
+}
+
+Sale.decorators = {}
+
+Sale.decorators.fedtax = {
+    getPrice:function(price){
+        var price = price += price*5/100
+        return price
+    }
+}
+
+Sale.decorators.quebec = {
+    getPrice:function(price){
+        return price += price*7.5/100
+    }
+}
+
+Sale.decorators.money = {
+    getPrice:function(price){
+        return '$' + price
+    }
+}
+
+Sale.decorators.cdn = {
+    getPrice:function(price){
+        return 'CDN$' + price
+    }
+}
+
+Sale.prototype.decorate = function(decorater){
+    this.decorators_list.push(decorater)
+}
+Sale.prototype.getPrice = function(){
+    var price = this.price,
+        i,
+        max = this.decorators_list.length,
+        name;
+    for(i=0;i<max;i++){
+        name = this.decorators_list[i]
+        price = Sale.decorators[name].getPrice(price)
+    }
+    return price
+}
+
+var sale = new Sale(100)
+sale.decorate('fedtax')
+sale.decorate('quebec')
+sale.decorate('money')
+sale.getPrice()  // $112.88
